7da6bd036ec7b04f75bb2398e00e2c1a5363af4f
Skullywag <mjskull@gmail.com>
2016-10-02 09:54:59 +0100

    initial commit

---
+++ Source/AlphaAnimals/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen ber eine Assembly werden ber die folgenden 
+// Attribute gesteuert. ndern Sie diese Attributwerte, um die Informationen zu ndern,
+// die mit einer Assembly verknpft sind.
+[assembly: AssemblyTitle("AlphaAnimals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlphaAnimals")]
+[assembly: AssemblyCopyright("Copyright   2016 (Skullywag)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// fr COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen mssen, legen Sie das ComVisible-Attribut fr diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt fr COM verfgbar gemacht wird
+[assembly: Guid("a8e081b6-8768-4fe9-817f-f0ee42cf13ef")]
+
+// Versionsinformationen fr eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie knnen alle Werte angeben oder die standardmigen Build- und Revisionsnummern 
+// bernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
